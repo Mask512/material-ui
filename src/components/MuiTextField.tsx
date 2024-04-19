49d6f0c3e89cdc4be46f2bc26d5ef0cfe655b340
Mask512 <mask.roktz@gmail.com>
2024-04-19 15:21:13 +0700

    text field pt.2 error state pre/suffix

--- src/components/MuiTextField.tsx
+++ src/components/MuiTextField.tsx
@@ -1,6 +1,9 @@
-import { Stack, TextField, Typography } from '@mui/material';
+import { InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export const MuiTextField = () => {
+  const [value, setValue] = useState('');
+
   return (
     <>
       <Typography marginBlock={'1rem'} variant="h6">
@@ -16,11 +19,39 @@ export const MuiTextField = () => {
           <TextField label="Small secondary" size="small" color="secondary" />
         </Stack>
         <Stack direction={'row'} spacing={2}>
-          <TextField label="Form Input" required />
+          <TextField
+            label="Form Input"
+            required
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            error={!value} // error state
+            helperText={!value ? 'Required' : 'Don\'t share your password to anyone'}
+          />
           <TextField label="Form Input" helperText="Dont share your password" />
           <TextField label="Password" type="password" />
           <TextField label="Disabled" disabled />
-          <TextField label="Read Only" value={'cannot edit this'} InputProps={{readOnly: true}} />
+          <TextField
+            label="Read Only"
+            value={'cannot edit this'}
+            InputProps={{ readOnly: true }}
+          />
+        </Stack>
+        <Stack direction={'row'} spacing={2}>
+          \
+          <TextField
+            label="Amount"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            label="Weight"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+            }}
+          />
         </Stack>
       </Stack>
     </>
